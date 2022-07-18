50fc8659ffa6864f1282fc652285793e0b81b12c
danbiilee <danbi.db@gmail.com>
2022-07-18 11:44:49 +0900

    7.6 스냅샷 테스트 구현

--- unit-react/src/components/forms/tests/habitAddForm.test.js
+++ unit-react/src/components/forms/tests/habitAddForm.test.js
@@ -1,33 +1,41 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import renderer from 'react-test-renderer';
 import HabitAddForm from '../habitAddForm';
 
 describe('HabitAddForm', () => {
-  let input;
-  let button;
-  let onAdd;
-
-  beforeEach(() => {
-    onAdd = jest.fn();
-    render(<HabitAddForm onAdd={onAdd} />);
-    input = screen.getByPlaceholderText('Habit');
-    button = screen.getByText('Add');
+  it('renders', () => {
+    const component = renderer.create(<HabitAddForm onAdd={jest.fn()} />);
+    expect(component.toJSON()).toMatchSnapshot();
   });
 
-  it('calls onAdd when button is clicked and valid habit is entered', () => {
-    // When: input에 원하는 습관의 이름 타이핑 -> add 버튼 클릭
-    userEvent.type(input, 'New Habit');
-    userEvent.click(button);
+  describe('Form Submit', () => {
+    let input;
+    let button;
+    let onAdd;
 
-    // Then: onAdd가 input에 입력된 이름과 함께 호출되어야 함
-    expect(onAdd).toHaveBeenCalledWith('New Habit');
-  });
+    beforeEach(() => {
+      onAdd = jest.fn();
+      render(<HabitAddForm onAdd={onAdd} />);
+      input = screen.getByPlaceholderText('Habit');
+      button = screen.getByText('Add');
+    });
 
-  it('does not call onAdd when the habit is empty', () => {
-    userEvent.type(input, '');
-    userEvent.click(button);
+    it('calls onAdd when button is clicked and valid habit is entered', () => {
+      // When: input에 원하는 습관의 이름 타이핑 -> add 버튼 클릭
+      userEvent.type(input, 'New Habit');
+      userEvent.click(button);
 
-    expect(onAdd).toHaveBeenCalledTimes(0);
+      // Then: onAdd가 input에 입력된 이름과 함께 호출되어야 함
+      expect(onAdd).toHaveBeenCalledWith('New Habit');
+    });
+
+    it('does not call onAdd when the habit is empty', () => {
+      userEvent.type(input, '');
+      userEvent.click(button);
+
+      expect(onAdd).toHaveBeenCalledTimes(0);
+    });
   });
 });
